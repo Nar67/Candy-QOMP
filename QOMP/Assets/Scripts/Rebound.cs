5a5057d6595df2a4b73d62b44776d9dbacbcde80
YaizaCano <yaiza.cano@est.fib.upc.edu>
2020-11-23 12:29:00 +0100

    l'eix z va per lliure, jo no entenc res

---
+++ QOMP/Assets/Scripts/Rebound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rebound : MonoBehaviour
+{
+    
+    public float speedy = 7.0f;
+    public float speedx = -3.0f;
+    public bool stuck;
+    private float dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stuck = true;
+        dir = speedy;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stuck)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                stuck = false;
+                transform.Translate(speedx * Time.deltaTime, dir * Time.deltaTime, 0.0f);
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                dir *= -1;
+            }
+            transform.Translate(speedx * Time.deltaTime, dir * Time.deltaTime, 0.0f);
+        }
+            
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Collision Out: " + gameObject.name);
+        speedx *= -1;
+    }
+}
---
+++ QOMP/Assets/Scripts/TrackBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackBall : MonoBehaviour
+{
+    [SerializeField] private GameObject ball;
+    public float speed = 7.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!ball.GetComponent<Rebound>().stuck) {
+            if (ball.transform.position.y > transform.position.y)
+                transform.Translate(0.0f, speed * Time.deltaTime, 0.0f);
+            else
+                transform.Translate(0.0f, -speed * Time.deltaTime, 0.0f);
+        }
+        
+    }
+
+   
+}

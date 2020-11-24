049653657e1155d8c85810a1020ccb97a311eac0
Narcis Terrado <narcis.tg@gmail.com>
2020-11-24 19:45:09 +0100

    menu

---
+++ QOMP/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject CreditsMenu;
+    public GameObject InstructionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void PlayNowButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Level1");
+    }
+
+    public void CreditsButton()
+    {
+        // Show Credits Menu
+        MainMenu.SetActive(false);
+        InstructionsMenu.SetActive(false);
+        CreditsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+        InstructionsMenu.SetActive(false);
+        CreditsMenu.SetActive(false);
+    }
+
+
+    public void InstructionsMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(false);
+        InstructionsMenu.SetActive(true);
+        CreditsMenu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+}

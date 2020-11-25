4bd8e2889bb622f72e0b8e33970e7acc6e41a2b2
YaizaCano <yaiza.cano@est.fib.upc.edu>
2020-11-25 14:19:03 +0100

    no havia afegit les textures

---
+++ QOMP/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    public bool key;
+    public Material mat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (key)
+        {
+            this.GetComponent<MeshRenderer>().material = mat;
+            Destroy(this.GetComponent<BoxCollider2D>());
+        }
+        
+    }
+}

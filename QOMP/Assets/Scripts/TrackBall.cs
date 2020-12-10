b0e32c90016fbdfe7e911d7ab1a4c064295748b2
Narcis Terrado <narcis.tg@gmail.com>
2020-12-10 19:56:19 +0100

    lvl1 adjustments and cane movement fixed

--- QOMP/Assets/Scripts/TrackBall.cs
+++ QOMP/Assets/Scripts/TrackBall.cs
@@ -7,6 +7,9 @@ public class TrackBall : MonoBehaviour
     public GameObject ball;
     public float speed = 2.0f;
 
+    private bool collidedTop;
+    private bool collidedBot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +20,34 @@ public class TrackBall : MonoBehaviour
     {
         
         if (!ball.GetComponent<Rebound>().stuck) {
-            if (ball.transform.position.y > transform.position.y)
+            if (ball.transform.position.y > transform.position.y && !collidedTop)
+            {
                 transform.Translate(0.0f, speed * Time.deltaTime, 0.0f);
-            else
+                collidedBot = false;
+            }
+            else if(ball.transform.position.y <= transform.position.y && !collidedBot)
+            {
                 transform.Translate(0.0f, -speed * Time.deltaTime, 0.0f);
+                collidedTop = false;
+            }
         }
         
     }
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "Tiles")
+        {
+            Vector2 contactPoint = collision.contacts[0].point;
+            if(contactPoint.y > transform.position.y)
+            {
+                collidedTop = true;
+            }
+            else
+            {
+                collidedBot = true;
+            }
+        }
+    }
    
 }
